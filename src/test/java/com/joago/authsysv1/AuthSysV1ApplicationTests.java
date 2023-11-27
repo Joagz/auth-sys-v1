96f2bbd4bfbf2d43bf5ca47c3650002c646feab7
joagz <joagomez.dev@gmail.com>
2023-11-27 15:27:28 -0300

    configured DataSource and created the models

---
+++ src/test/java/com/joago/authsysv1/AuthSysV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joago.authsysv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSysV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
